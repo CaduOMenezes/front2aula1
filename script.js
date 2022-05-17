573b7a265e6a94ae087f700e9f63a960ced013ec
CaduOMenezes <dudumneto@hotmail.com>
2022-05-17 20:16:06 -0300

    codigo da aula 1 de front 2 depois de correção

--- script.js
+++ script.js
@@ -21,8 +21,8 @@ let sum = 0;
 
 for (let i = 0; i < array.length; i++) {
     sum += array[i];
-}
-console.log("Teste Kev:")
+};
+console.log("Teste Kev:");
 console.log(sum);
 
 
@@ -40,33 +40,33 @@ function soma(arrayDeNumeros) {
         return soma;
     }
 
-}
+};
 
-console.log(soma(numeros))
+console.log(soma(numeros));
 
 
 //Leandro:
-const array = [1, 2, 3, 4];
-let sum = 0
+const array2 = [1, 2, 3, 4];
+let sum2 = 0
 
-for (let i = 0; i < array.length; i++) {
-    sum += array[i];
+for (let i = 0; i < array2.length; i++) {
+    sum2 += array2[i];
 }
 console.log("Leandro_Teste")
-console.log(sum);
+console.log(sum2);
 
 
 
 
 
 //Diego
-let numeros = [1, 2, 4,8]
+let numeros2 = [1, 2, 4,8]
 function somar (array){
     let total = 0;
-    for (let i = 0; i < numeros.length; i++) {
-        total += numeros[i];
+    for (let i = 0; i < array.length; i++) {
+        total += array[i];
     }
     return total;
-}
-console.log("meuteste");
+};
+console.log("meu(diego)teste");
 console.log(somar(numeros));
